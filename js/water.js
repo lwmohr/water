04ffda8fadf47b1566017c223fb5d6e6b4422433
FreddyTheCrawdad <lmohr78+git@gmail.com>
2012-10-27 18:26:13 -0500

    gets data and graphs when site is selected

--- js/water.js
+++ js/water.js
@@ -1,5 +1,84 @@
 var elevationArray = new Array();
 var noElevation = 998877.000;
+	getData = function() {
+		elevationArray = new Array();
+		myurl = 'http://www.usbr.gov/gp-bin/arcread.pl?jsonp=?';
+		mysite = $("#siteSelect").val();
+		myparam = $("#parameterSelect").val();
+		$.getJSON(myurl, {
+			st : mysite,
+			by : "2002",
+			bm : "1",
+			bd : "1",
+			ey : "2012",
+			em : "11",
+			ed : "3",
+			pa : myparam
+		}, function(data) {
+			for (var i = 0; i < data.SITE.DATA.length; i++) {
+				elevation = parseFloat(data.SITE.DATA[i].FB);
+				if ((elevation != noElevation) && (elevation > 0)) {
+					day = new Date(Date.parse(data.SITE.DATA[i].DATE));
+					elevationArray.push({
+						"day" : day,
+						"elevation" : elevation
+					});
+				}
+			}
+			if (elevationArray.length > 0) {
+				$("#noData").hide();
+			} else {
+				$("#noData").show();
+			}
+		insertGraph();
+
+		});
+	}
+
+	
+	insertGraph = function() {
+		$("#graph").empty();
+
+		var margin = {
+			top : 20,
+			right : 20,
+			bottom : 30,
+			left : 50
+		}, width = $("#graph").width() - margin.left - margin.right, height = $(document).height() - margin.top - margin.bottom - 105;
+
+		var parseDate = d3.time.format("%d-%b-%y").parse;
+
+		var x = d3.time.scale().range([0, width]);
+
+		var y = d3.scale.linear().range([height, 0]);
+
+		var xAxis = d3.svg.axis().scale(x).orient("bottom");
+
+		var yAxis = d3.svg.axis().scale(y).orient("left");
+
+		var area = d3.svg.area().x(function(d) {
+			return x(d.day);
+		}).y0(height).y1(function(d) {
+			return y(d.elevation);
+		});
+
+		var svg = d3.select("#graph").append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+		console.log(elevationArray);
+		x.domain(d3.extent(elevationArray, function(d) {
+			return d.day;
+		}));
+		y.domain(d3.extent(elevationArray, function(d) {
+			return d.elevation;
+		}));
+
+		svg.append("path").datum(elevationArray).attr("class", "area").attr("d", area);
+
+		svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")").call(xAxis);
+
+		svg.append("g").attr("class", "y axis").call(yAxis).append("text").attr("dy", ".71em").style("text-anchor", "end").text("Elev.");
+	}
+
+
 $(document).ready(function() {
 
 	// when the site dropdown changes, update the parameters.
@@ -34,6 +113,7 @@ $(document).ready(function() {
 			});
 
 		}
+		getData();
 	});
 
 	$("#parameterSelect").change(function() {
@@ -96,80 +176,6 @@ $(document).ready(function() {
 	 element.appendChild(parameterOption);
 	 }
 	 */
-	getData = function() {
-		elevationArray = new Array();
-		myurl = 'http://www.usbr.gov/gp-bin/arcread.pl?jsonp=?';
-		mysite = $("#siteSelect").val();
-		myparam = $("#parameterSelect").val();
-		$.getJSON(myurl, {
-			st : mysite,
-			by : "2002",
-			bm : "1",
-			bd : "1",
-			ey : "2012",
-			em : "11",
-			ed : "3",
-			pa : myparam
-		}, function(data) {
-			for (var i = 0; i < data.SITE.DATA.length; i++) {
-				elevation = parseFloat(data.SITE.DATA[i].FB);
-				if ((elevation != noElevation) && (elevation != 0)) {
-					day = new Date(Date.parse(data.SITE.DATA[i].DATE));
-					elevationArray.push({
-						"day" : day,
-						"elevation" : elevation
-					});
-				}
-			}
-			if (elevationArray.length > 0) {
-				$("#noData").hide();
-			} else {
-				$("#noData").show();
-			}
-
-		});
-	}
 	// Graph it
-	insertGraph = function() {
-		$("#graph").empty();
-
-		var margin = {
-			top : 20,
-			right : 20,
-			bottom : 30,
-			left : 50
-		}, width = $("#graph").width() - margin.left - margin.right, height = $(document).height() - margin.top - margin.bottom - 105;
-
-		var parseDate = d3.time.format("%d-%b-%y").parse;
-
-		var x = d3.time.scale().range([0, width]);
-
-		var y = d3.scale.linear().range([height, 0]);
-
-		var xAxis = d3.svg.axis().scale(x).orient("bottom");
-
-		var yAxis = d3.svg.axis().scale(y).orient("left");
-
-		var area = d3.svg.area().x(function(d) {
-			return x(d.day);
-		}).y0(height).y1(function(d) {
-			return y(d.elevation);
-		});
-
-		var svg = d3.select("#graph").append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-		console.log(elevationArray);
-		x.domain(d3.extent(elevationArray, function(d) {
-			return d.day;
-		}));
-		y.domain(d3.extent(elevationArray, function(d) {
-			return d.elevation;
-		}));
-
-		svg.append("path").datum(elevationArray).attr("class", "area").attr("d", area);
-
-		svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")").call(xAxis);
-
-		svg.append("g").attr("class", "y axis").call(yAxis).append("text").attr("dy", ".71em").style("text-anchor", "end").text("Elev.");
-	}
 });
 
